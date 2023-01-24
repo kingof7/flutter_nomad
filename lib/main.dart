be7d45dfaa3968d03767071ac8ce7903231a610c
vlfl0 <vlfl0@naver.com>
2023-01-24 17:22:40 +0900

    #2.5 classes recap

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 // Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-
-import 'dart:html';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class Player {
+  String? name; // ? : name을 가질수도, 아닐수도
+  // Player({required this.name});
+  Player();
+}
+
 void main() {
+  // var nico = Player(name: "potato");
+  var nico = Player();
   runApp(App());
 }
 
