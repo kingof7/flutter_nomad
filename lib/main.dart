3fa0b1ba2b149a72d0534a4b3027b39df93c3ffc
vlfl0 <vlfl0@naver.com>
2023-02-04 20:26:44 +0900

    #3.2 Buttons Section (10:42)

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class App extends StatelessWidget {
           body: Padding(
             padding: EdgeInsets.symmetric(horizontal: 40),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(
                   height: 80,
@@ -46,12 +47,58 @@ class App extends StatelessWidget {
                           Text(
                             'Welcome back',
                             style: TextStyle(
-                                color: Colors.white.withOpacity(0.8),
+                                color: Color.fromRGBO(255, 255, 255, 0.8),
                                 fontSize: 18),
                           ),
                         ])
                   ],
                 ),
+                SizedBox(
+                  height: 120,
+                ),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white.withOpacity(0.8),
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  '\$5 194 482',
+                  style: TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white.withOpacity(0.8),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(45),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 50,
+                        ),
+                        child: Text(
+                          'Transfer',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
               ],
             ),
           )),
